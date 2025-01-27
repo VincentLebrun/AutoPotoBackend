28746874a8aed5c2a38480c9706eb275fe500b14
Vincent-lebrun <lebrunvincent12@gmail.com>
2025-01-27 14:41:37 +0100

    feat(ATP-002): add golang ci linter try to test

--- internals/db/db.go
+++ internals/db/db.go
@@ -1,1 +1,22 @@
-// ici la connexion à la base de donnée 
+package db
+
+import (
+	"database/sql"
+	"os"
+
+	"github.com/go-sql-driver/mysql"
+)
+
+var db *sql.DB
+
+func InitDb() {
+	cfg := mysql.Config{
+		dbHost: os.Getenv("DB_HOST"),
+		dbPort: os.Getenv("DB_ENV"),
+		dbUser: os.Getenv("DB_USER"),
+		dbPass: os.Getenv("DB_PASSWORD"),
+		dbName: os.Getenv("DB_NAME"),
+	}
+
+	db, err = sql.Open("mysql", cfg.FormatDSN())
+}
